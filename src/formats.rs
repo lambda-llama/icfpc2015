c85b8845e6c33ec2494980bd4ea215ca0eac4f6d
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-08-07 18:09:35 +0300

    add linear congugate generator

--- src/formats.rs
+++ src/formats.rs
@@ -10,6 +10,23 @@ pub struct Board {
     pub sourceSeeds: Vec<u64>
 }
 
+fn extract(x: u64) -> usize {
+    ((x % (1 << 31)) >> 16)  as usize
+}
+
+pub fn get_source_seq(length: usize, seed: u64) -> Vec<usize> {
+    let modulus: u64 = 1 << 32;
+    let multiplier: u64 = 1103515245;
+    let increment: u64 = 12345;
+    let mut result = Vec::new();
+    let mut c = seed;
+    for _ in 0..length {
+        result.push(extract(c));
+        c = (c * multiplier + increment) % modulus;
+    }
+    result
+}
+
 #[derive(Debug, RustcDecodable, RustcEncodable)]
 pub struct Unit {
     pub members: Vec<Cell>,
