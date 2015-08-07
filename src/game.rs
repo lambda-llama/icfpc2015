c18169a267a5f7c1dfc1f2ea07feb450af875a57
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-08-07 16:30:57 +0300

    add code for movement

--- src/game.rs
+++ src/game.rs
@@ -6,18 +6,36 @@ struct Board {
     cells: Vec<Vec<bool>>
 }
 
+#[derive(Clone)]
 struct UnitShape {
     cells: Vec<Pos>,
     pivot: Pos
 }
 
+
+enum Move {
+    Translation(Pos),
+    Rotation(i8)
+}
+
+#[derive(Clone)]
 struct Unit {
     shape: UnitShape,
     position: Pos,               // pivot positon
     rotation: u8                 // 0..5
 }
 
-enum Move {
-    Translation(Pos),
-    Rotation(i8)
+impl Unit {
+    pub fn moved(self, m: &Move) -> Unit {
+        match m {
+            &Move::Translation((dx, dy)) => Unit {
+                position: (self.position.0 + dx, self.position.1 + dy),
+                ..self
+            },
+            &Move::Rotation(amount) => Unit {
+                rotation: (((self.rotation as i32) + amount as i32) % 6) as u8,
+                ..self
+            }
+        }
+    }
 }
