4527e8e65d21beb5a8ec20781298b60089f7200c
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-08-08 00:29:34 +0300

    minor rename

--- src/board.rs
+++ src/board.rs
@@ -29,7 +29,7 @@ impl Board {
     /// Returns `true` if a `unit` is within board boundaries and does
     /// not overlap any of the occupied cells.
     pub fn check_unit_position(&self, unit: &Unit) -> bool {
-        unit.cells.iter().all(|c| self.is_valid(c.x, c.y) && !self.at(c.x, c.y))
+        unit.cells.iter().all(|c| self.is_valid(c.x, c.y) && self.is_free(c.x, c.y))
     }
 
     pub fn get_correct_commands(&self, unit: &Unit) -> Vec<&Command> {
@@ -38,15 +38,15 @@ impl Board {
         }).collect()
     }
 
-    pub fn at(&self, x: i32, y: i32) -> bool {
+    pub fn is_free(&self, x: i32, y: i32) -> bool {
         assert!(self.is_valid(x, y));
-        self.cells[y as usize][x as usize]
+        !self.cells[y as usize][x as usize]
     }
 
     pub fn place_unit(&self, unit: &Unit) -> Board {
         let mut cells = (*self.cells).clone();
         for cell in unit.cells.iter() {
-            assert!(!self.at(cell.x, cell.y));
+            assert!(self.is_free(cell.x, cell.y));
             cells[cell.y as usize][cell.x as usize] = true;
         }
         Board {
